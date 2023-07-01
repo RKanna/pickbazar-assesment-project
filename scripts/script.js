27e6033a8cde963e782e95d5506bd67b94b766cd
RKanna <rkannan.balakrishnan@hotmail.com>
2023-07-02 01:45:10 +0530

    js for cartbox and nav searchbar and btn

--- scripts/script.js
+++ scripts/script.js
@@ -52,3 +52,61 @@ window.addEventListener("scroll", function () {
     sideBar.classList.remove("position-list-box2");
   }
 });
+
+let navSearchElement = document.getElementById("nav-search-element");
+navSearchElement.style.display = "none";
+let rightNavElements = document.getElementById("right-nav-element");
+rightNavElements.style.marginLeft = "400px";
+let navBar = document.getElementById("navbar");
+window.addEventListener("scroll", function () {
+  let navSearchElement = document.getElementById("nav-search-element");
+  let heroSearchBar = document.getElementById("hero-searchbar");
+  // var scrollPosition = window.scrollY || window.pageYOffset;
+  let scrollPosition = window.scrollY;
+
+  let shopContainerTop = heroSearchBar.offsetTop;
+  let shopContainerHeight = heroSearchBar.offsetHeight;
+
+  if (scrollPosition >= shopContainerTop) {
+    navSearchElement.style.display = "flex";
+    rightNavElements.style.marginLeft = "-90px";
+    navBar.classList.add("navbar-border");
+    navBar.style.backgroundColor = "white";
+  } else {
+    navSearchElement.style.display = "none";
+    rightNavElements.style.marginLeft = "400px";
+    navBar.classList.remove("navbar-border");
+    navBar.style.backgroundColor = "#fafafa";
+  }
+
+  if (scrollPosition >= shopContainerTop + shopContainerHeight) {
+    navSearchElement.style.display = "flex";
+  }
+});
+
+window.addEventListener("click", function () {
+  let navSearchBar = document.getElementById("nav-searchbar");
+  let navSearchButton = document.getElementById("nav-search-button");
+  navSearchBar.classList.add("search-nav-clicked");
+  navSearchButton.classList.add("nav-search-btn-clicked");
+});
+
+window.addEventListener("scroll", function () {
+  let cartBox = document.getElementById("cartbox");
+  let sliderContainer = document.getElementById("slider-container");
+  // var scrollPosition = window.scrollY || window.pageYOffset;
+  let scrollPosition = window.scrollY;
+
+  let shopContainerTop = cartBox.offsetTop;
+  let shopContainerHeight = cartBox.offsetHeight;
+
+  if (scrollPosition >= shopContainerTop) {
+    sliderContainer.classList.add("slider-z-index");
+  } else {
+    sliderContainer.classList.remove("slider-z-index");
+  }
+
+  if (scrollPosition >= shopContainerTop + shopContainerHeight) {
+    sliderContainer.classList.add("slider-z-index");
+  }
+});
